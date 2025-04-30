ef45be0ab43f35d17b7fefda38b687081b1fde36
caioo <matossousacaio.cms@gmail.com>
2025-04-30 11:15:16 -0300

    refactor - mudado de Byte[] para byte[]

--- src/main/java/com/caioosm/qrcodegenerator/ports/StoragePort.java
+++ src/main/java/com/caioosm/qrcodegenerator/ports/StoragePort.java
@@ -1,5 +1,5 @@
 package com.caioosm.qrcodegenerator.ports;
 
 public interface StoragePort {
-    String uploadFile(Byte[] fileData, String fileName, String contentType);
+    String uploadFile(byte[] fileData, String fileName, String contentType);
 }
