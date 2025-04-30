80031b863e2a6aca8f384ae3c11f6c66d7b02813
caioo <matossousacaio.cms@gmail.com>
2025-04-30 11:32:02 -0300

    refact - logica controller

--- src/main/java/com/caioosm/qrcodegenerator/controller/QrCodeController.java
+++ src/main/java/com/caioosm/qrcodegenerator/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.caioosm.qrcodegenerator.controller;
 
 import com.caioosm.qrcodegenerator.dto.qrcode.QrCodeGenerateRequest;
 import com.caioosm.qrcodegenerator.dto.qrcode.QrCodeGenerateResponse;
+import com.caioosm.qrcodegenerator.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,9 +11,20 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generateQrCode(@RequestBody QrCodeGenerateRequest qrCodeGenerateRequest) {
-        return null;
+        try {
+            QrCodeGenerateResponse response =  this.qrCodeGeneratorService.generateAndUploadQrCode(qrCodeGenerateRequest.text());
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
 }
